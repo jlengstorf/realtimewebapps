5b57737de861cc424d7fe881a04739a6fe25dfd5
jlengstorf <jason.lengstorf@copterlabs.com>
2012-09-18 21:43:59 -0700

    Fixed a formatting issue.

--- system/views/question.inc.php
+++ system/views/question.inc.php
@@ -1,9 +1,9 @@
 
         <li data-id="<?php echo $question_id; ?>" 
             data-count="<?php echo $vote_count; ?>">
-            <?php echo $answer_link; ?>
+            <?php echo $answer_link; ?> 
             <p>
                 <?php echo $question; ?> 
             </p>
-            <?php echo $vote_link; ?>
+            <?php echo $vote_link; ?> 
         </li>
