95179b1108fa518921a6a84b74786def9d2e482c
jlengstorf <jason.lengstorf@copterlabs.com>
2013-03-17 11:12:03 -0700

    Fixed layout issues and retrofitted reworked code from the app back into the HTML/CSS. Things that are missing from the live stylesheet: transitions, error page styles, and styles for answered/voted questions. Those will be introduced in Chapter 8/9.

--- system/views/room-closed.inc.php
+++ system/views/room-closed.inc.php
@@ -1,6 +1,6 @@
 
     <h3>This session has ended.</h3>
     <p>
-        If you have a question that wasn't answered, please 
+        If you have a question that wasn&rsquo;t answered, please 
         <a href="mailto:<?php echo $email; ?>">email the presenter</a>.
     </p>
